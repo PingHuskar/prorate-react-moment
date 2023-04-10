b80d0606fd9b79f7b0af68641869f21bc137344c
Chadin Chaipornpisuth <ping47024@icloud.com>
2023-04-10 22:08:23 +0700

    fix build error

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import './App.css'
 import moment from 'moment'
 
 function App() {
+  const numToday: number = parseInt(moment().format('D'))
 
   return (
     <div className="App">
@@ -9,7 +10,7 @@ function App() {
         คำนวน Prorate รายวัน
       </h1>
       <h2 id='prorate'>
-        {(moment().format('D')/moment().daysInMonth()*100).toFixed(2)} %
+        {(numToday/moment().daysInMonth()*100).toFixed(2)} %
       </h2>
       <h3>of {moment().format('MMMM')} {moment().format('YYYY')}</h3>
       <p>MS Excel : https://web.facebook.com/reportingengineer/posts/761079775384860</p>
